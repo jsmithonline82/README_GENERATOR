f884603c29b321a76b54fae9147616ce004b1ea1
Jackson Smith <jsmithbootcamp2020@gmail.com>
2020-11-10 15:43:50 -0500

    Working on function for prompts

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 const axios = require("axios");
-const generate = require('./utils/generateMarkdown');
+// const generate = require('./utils/generateMarkdown');
 
 const questions = [
     {
@@ -55,3 +55,34 @@ const questions = [
         message: "What is your repo link?"
     },
 ];
+
+inquirer
+    .prompt(questions)
+    .then(function(data){
+        const queryUrl = `https://api.github.com/users/${data.username}`;
+
+        axios.get(queryUrl).then(function(res) {
+            
+            const githubInfo = {
+                githubImage: res.data.avatar_url,
+                email: res.data.email,
+                profile: res.data.html_url,
+                name: res.data.name
+            };
+            
+          fs.writeFile("README.md", generate(data, githubInfo), function(err) {
+            if (err) {
+              throw err;
+            };
+    
+            console.log("New README file created with success!");
+          });
+        });
+
+});
+
+function init() {
+
+}
+
+init();
